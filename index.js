b71eb671c10b5e65f06373a9a2c7f373e1c88759
Ranjeet Kumar Raj <121329638+RanjeetRaj444@users.noreply.github.com>
2023-09-25 19:28:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", userRoutes);
-app.use("/api", auth_middleware, employeeRoutes);
+app.use("/api", employeeRoutes);
 
 app.listen(process.env.PORT, async () => {
 	try {
